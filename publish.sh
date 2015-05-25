6df2b334ca5dba95456379a428e8f8a5b924c202
gdrouet <guidrouet@gmail.com>
2015-05-25 13:57:41 +0200

    Workaround for https://github.com/wuic/wuic/issues/186.

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mvn clean install && \
-echo "cp -r target/site/* . && \git add . && \git commit -m 'Publish processed sources from \"sources\" branch.' && \git checkout sources" > target/finish.sh && \
+echo "cp -r target/site/* . && cp -r target/filtered/images/* . && \git add . && \git commit -m 'Publish processed sources from \"sources\" branch.' && \git checkout sources" > target/finish.sh && \
 git checkout master
