ae820aaf25fdfa171dbe05a003931ea7b50088fb
gdrouet <guidrouet@gmail.com>
2015-06-07 12:42:02 +0200

    Removes old files.

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mvn clean install && \
-echo "cp -r target/site/* . && mkdir -p images && cp -r target/filtered/images/* images && \git add . && \git commit -m 'Publish processed sources from \"sources\" branch.' && \git checkout sources" > target/finish.sh && \
+echo "git rm -r pages && cp -r target/site/* . && mkdir -p images && cp -r target/filtered/images/* images && \git add . && \git commit -m 'Publish processed sources from \"sources\" branch.' && \git checkout sources" > target/finish.sh && \
 git checkout master
